52995d3fe9c1e71e3711c653670cbf1f13bb3152
Lennart Buhl <git@hackmate.de>
2013-09-14 07:52:03 +0200

    swap MISSIONS and INTEL tabs (niantic did that with 1.35)

--- src/broot/ingress/mod/AboutModActivity.java
+++ src/broot/ingress/mod/AboutModActivity.java
@@ -56,7 +56,7 @@ public class AboutModActivity extends BaseSubActivity {
                         updateTabsValues(false);
                     }
                 });
-                tabsItem.addButton("ITEMS", "Show", new ClickListener() {
+                tabsItem.addButton("INVENTORY", "Show", new ClickListener() {
                     @Override
                     public void clicked(InputEvent event, float x, float y) {
                         Config.showOrigItemsTab = !Config.showOrigItemsTab;
@@ -70,17 +70,17 @@ public class AboutModActivity extends BaseSubActivity {
                         updateTabsValues(true);
                     }
                 });
+                tabsItem.addButton("MISSIONS", "Show", new ClickListener() {
+                    @Override
+                    public void clicked(InputEvent event, float x, float y) {
+                        Config.showMissionTab = !Config.showMissionTab;
+                        updateTabsValues(true);
+                    }
+                });
                 tabsItem.addButton("INTEL", "Show", new ClickListener() {
                     @Override
                     public void clicked(InputEvent event, float x, float y) {
                         Config.showIntelTab = !Config.showIntelTab;
-                        updateTabsValues(true);
-                    }
-                });
-                tabsItem.addButton("MISSION", "Show", new ClickListener() {
-                    @Override
-                    public void clicked(InputEvent event, float x, float y) {
-                        Config.showMissionTab = !Config.showMissionTab;
                         updateTabsValues(true);
                     }
                 });
@@ -292,8 +292,8 @@ public class AboutModActivity extends BaseSubActivity {
         tabsItem.buttons.get(0).setText(Config.itemsTab.desc);
         tabsItem.buttons.get(1).setText(Config.showOrigItemsTab ? "Show" : "Hide");
         tabsItem.buttons.get(2).setText(Config.showAgentTab ? "Show" : "Hide");
-        tabsItem.buttons.get(3).setText(Config.showIntelTab ? "Show" : "Hide");
-        tabsItem.buttons.get(4).setText(Config.showMissionTab ? "Show" : "Hide");
+        tabsItem.buttons.get(3).setText(Config.showMissionTab ? "Show" : "Hide");
+        tabsItem.buttons.get(4).setText(Config.showIntelTab ? "Show" : "Hide");
         tabsItem.buttons.get(5).setText(Config.showRecruitTab ? "Show" : "Hide");
         tabsItem.buttons.get(6).setText(Config.showPasscodeTab ? "Show" : "Hide");
         tabsItem.buttons.get(7).setText(Config.showDeviceTab ? "Show" : "Hide");
